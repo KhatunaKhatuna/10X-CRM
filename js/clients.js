7031554a77e0ff870da0398716dc0ccf7b8fef88
KhatunaKhatuna <kh.chitishvili@gmail.com>
2026-07-22 16:22:39 +0400

    feat: setup clients html layout
    - Create clients.html with shared sidebar and header
    
    - Initialize empty clients.js and clients.css files

---
+++ js/clients.js
@@ -0,0 +1,7 @@
+/**
+ * Clients Module Logic (P4)
+ * Phase 2 will be implemented here (fetching data and storing it).
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Clients module loaded.");
+});
